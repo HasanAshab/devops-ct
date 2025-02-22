e4ebf6ba4a1d14e5401583bd5491a718487f9368
Hasan Ashab <110120731+HasanAshab@users.noreply.github.com>
2025-02-22 23:01:35 +0600

    Initial commit

---
+++ api/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+
+
+urlpatterns = [
+    path(
+        "api/",
+        include("api.docs.urls"),
+    ),
+    path(
+        "api/",
+        include("api.authentication.urls"),
+    ),
+    path(
+        "api/",
+        include("api.accounts.urls"),
+    ),
+    path(
+        "api/",
+        include("api.users.urls"),
+    ),
+]
