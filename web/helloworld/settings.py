28d231c600355ff54cae52aa396c9e7bcbd20f3b
Your Name <you@example.com>
2025-03-18 12:31:03 +0600

    backup

--- web/helloworld/settings.py
+++ web/helloworld/settings.py
@@ -128,3 +128,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
+    'UNAUTHENTICATED_USER': None,
+}
